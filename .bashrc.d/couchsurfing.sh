2f563abb84af53b7cf31bab64b24624faf011e97
Duffy Tilleman <duffy.tilleman@couchsurfing.com>
2013-03-30 14:19:18 -0700

    only set csbox stuff on my MBP local dev machine

--- .bashrc.d/couchsurfing.sh
+++ .bashrc.d/couchsurfing.sh
@@ -1,11 +1,17 @@
-# setup couchsurfing stuff
-prevdir=$PWD
-cd $HOME/csbox
-source $HOME/csbox/sh_csbox.sh
-cd $prevdir
-export RAILS_ENV=development
+mbphostname="duffy.local"
+if [ "$(hostname)" == $mbphostname ]
+then
+  # setup couchsurfing stuff
+  prevdir=$PWD
+  cd $HOME/csbox
+  source $HOME/csbox/sh_csbox.sh
+  cd $prevdir
+  export RAILS_ENV=development
 
-# not sure why this needs to be done repeadetly
-ssh-add $HOME/.ssh/id_rsa
+  # not sure why this needs to be done repeadetly
+  ssh-add $HOME/.ssh/id_rsa
+else
+  echo "Not sourcing couchsufring.sh because hostname '$(hostname)' !== '$mbphostname'"
+fi
 
 
