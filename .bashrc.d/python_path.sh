a44c8615f9bb63219f59da109b25e44fd15ab9eb
Duffy Tilleman <duffy.tilleman@gmail.com>
2014-07-02 09:46:34 -0700

    fix plethora pythonpath

--- .bashrc.d/python_path.sh
+++ .bashrc.d/python_path.sh
@@ -1,3 +1,2 @@
-PLETHORA_HOME=$HOME/Development/plethora
-MA_HOME=$PLETHORA_HOME/machine-automation/python
-export PYTHONPATH=$MA_HOME:$PLETHORA_HOME/api_client:$MA_HOME/utils:$MA_HOME/gcode
+export PLETHORA_HOME=$HOME/Development/plethora
+export PYTHONPATH=$PLETHORA_HOME/api_client
