3c4d2df320c90f5f0691555897ea1586b139f95a
Duffy Tilleman <duffy.tilleman@gmail.com>
2014-11-13 13:18:19 -0800

    more robust tmpdir creation

--- .bashrc.d/git-config-check.sh
+++ .bashrc.d/git-config-check.sh
@@ -1,13 +1,19 @@
 CURRENT_DIR=$PWD
 
-TMPDIR=`tempfile`
-rm $TMPDIR
-mkdir $TMPDIR
-cd $TMPDIR
+if which tempfile; then
+  MYTMPDIR=`tempfile`
+  rm $MYTMPDIR
+else
+  MYTMPDIR=$TMPDIR/git-config-check
+fi
+
+mkdir $MYTMPDIR
+
+cd $MYTMPDIR
 git init > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
   git config --global push.default nothing
 fi
 cd $CURRENT_DIR
-rm -rf $TMPDIR
+rm -rf $MYTMPDIR
