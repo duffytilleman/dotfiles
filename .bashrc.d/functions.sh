1d0645247611fe2dc3e6cb682a51ea61ab249e2a
Duffy Tilleman <duffy.tilleman@gmail.com>
2015-02-11 15:43:51 -0800

    Fix bug in 'latest' shell function

--- .bashrc.d/functions.sh
+++ .bashrc.d/functions.sh
@@ -5,8 +5,8 @@ supa () {
 }
 
 latest () {
-  if [ -d $1 ]; then
-    local res=$(ls -Art $1 | tail -n 1);
+  if [ -d "$1" ]; then
+    local res=$(ls -Art "$1" | tail -n 1);
     echo $1/$res;
   fi
 }
