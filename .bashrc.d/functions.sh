4b9fcd2868e38ef123779638fb4ad41fcdeedbf9
Duffy Tilleman <duffy.tilleman@gmail.com>
2015-07-30 11:36:40 -0700

    Improvement to the "latest" shell function

--- .bashrc.d/functions.sh
+++ .bashrc.d/functions.sh
@@ -7,6 +7,9 @@ supa () {
 latest () {
   if [ -d "$1" ]; then
     local res=$(ls -Art "$1" | tail -n 1);
-    echo $1/$res;
+    if [[ ! $1 =~ /$ ]]; then
+      echo $1/$res
+    fi
+    echo $1$res;
   fi
 }
